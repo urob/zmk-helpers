2b13dcb1c68f33bc88e5d65aa25f80f53a03bf0f
Theo Lemay <16546293+theol0403@users.noreply.github.com>
2023-04-19 22:34:56 -0400

    Add support for dynamic macros
    This adds the proposed definition from https://github.com/zmkfirmware/zmk/pull/1351

--- helper.h
+++ helper.h
@@ -11,14 +11,15 @@
 
 /* ZMK_BEHAVIOR */
 
-#define ZMK_BEHAVIOR_CORE_caps_word   compatible = "zmk,behavior-caps-word";  #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_hold_tap    compatible = "zmk,behavior-hold-tap";   #binding-cells = <2>
-#define ZMK_BEHAVIOR_CORE_key_repeat  compatible = "zmk,behavior-key-repeat"; #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_macro       compatible = "zmk,behavior-macro";      #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_mod_morph   compatible = "zmk,behavior-mod-morph";  #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_sticky_key  compatible = "zmk,behavior-sticky-key"; #binding-cells = <1>
-#define ZMK_BEHAVIOR_CORE_tap_dance   compatible = "zmk,behavior-tap-dance";  #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_tri_state   compatible = "zmk,behavior-tri-state";  #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_caps_word      compatible = "zmk,behavior-caps-word";      #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_hold_tap       compatible = "zmk,behavior-hold-tap";       #binding-cells = <2>
+#define ZMK_BEHAVIOR_CORE_key_repeat     compatible = "zmk,behavior-key-repeat";     #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_macro          compatible = "zmk,behavior-macro";          #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_dynamic_macro  compatible = "zmk,behavior-dynamic-macro";  #binding-cells = <1>
+#define ZMK_BEHAVIOR_CORE_mod_morph      compatible = "zmk,behavior-mod-morph";      #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_sticky_key     compatible = "zmk,behavior-sticky-key";     #binding-cells = <1>
+#define ZMK_BEHAVIOR_CORE_tap_dance      compatible = "zmk,behavior-tap-dance";      #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_tri_state      compatible = "zmk,behavior-tri-state";      #binding-cells = <0>
 
 #define ZMK_BEHAVIOR(name, type, ...) \
     / { \
