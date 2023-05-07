5e996b123756e216d1d9ea4e49ab4d12db8ec5eb
mateuszaaa <mateusz.cz.nowakowski@gmail.com>
2023-05-07 19:46:08 +0200

    Update keypos_kyria_50keys.h
    Resolving PR comment.

--- keypos_def/keypos_kyria_50keys.h
+++ keypos_def/keypos_kyria_50keys.h
@@ -3,7 +3,7 @@
   ╭────────────────────────────────┬───────────────────────────────╮  ╭─────────────────────────────────┬─────────────────────────────────╮
   │  0   1   2   3   4   5         │          6   7   8   9 10  11 │  │ LT5 LT4 LT3 LT2 LT1 LT0         │         RT0 RT1 RT2 RT3 RT4 RT5 │
   │ 12  13  14  15  16  17         │         18  19  20  21 22  23 │  │ LM5 LM4 LM3 LM2 LM1 LM0         │         RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 24  25  26  27  28  29  30  31 │ 32  33  34  35  36  37 38  39 │  │ LB7 LB6 LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 RB6 RB7 │
+  │ 24  25  26  27  28  29  30  31 │ 32  33  34  35  36  37 38  39 │  │ LB5 LB4 LB3 LB2 LB1 LB0 LF1 LF0 │ RF0 RF1 RB0 RB1 RB2 RB3 RB4 RB5 │
   ╰───────────╮ 40  41  42  43  44 │ 45  46  47  48  49 ╭──────────╯  ╰───────────╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───────────╯
               ╰────────────────────┴────────────────────╯                         ╰─────────────────────┴─────────────────────╯            
 */
@@ -44,8 +44,6 @@
 #define LB3 28
 #define LB4 27
 #define LB5 26
-#define LB6 25
-#define LB7 24
 
 #define RB0 32  // right-bottom row
 #define RB1 33
@@ -53,8 +51,6 @@
 #define RB3 35
 #define RB4 36
 #define RB5 37
-#define RB6 38
-#define RB7 39
 
 #define LH0 44  // left thumb keys
 #define LH1 43
@@ -67,4 +63,10 @@
 #define RH2 47
 #define RH3 48
 #define RH4 49
+
+#define LF1 25  // left extra keys
+#define LF0 24
+
+#define RF0 38  // right extra keys
+#define RF1 39
 
