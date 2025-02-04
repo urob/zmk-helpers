7361bf31b65a6632f11b7270a21897c723f01e95
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2025-02-05 06:48:18 +1100

    chore: revise the encoder code to follow Urob's convention

--- include/zmk-helpers/key-labels/eyelash42.h
+++ include/zmk-helpers/key-labels/eyelash42.h
@@ -1,5 +1,8 @@
 /*                42 KEY MATRIX with ENCODER and JOYSTICK / LAYOUT MAPPING
 
+based on Aliexpress variation of Foostan corne
+https://github.com/a741725193/zmk-new_corne/blob/main/README_EN.md
+
   ╭────────────────────────╮             ╭────╮      ╭─────────────────────────╮
   │  0   1   2   3   4   5 │         ╭───╯  6 ╰───╮  │   7   8   9  10  11  12 │
   │ 13  14  15  16  17  18 │ ╭────╮  │ 19  20  21 │  │  22  23  24  25  26  27 │
@@ -7,18 +10,16 @@
   ╰───────────╮ 42  43  44 │ ╰────╯      ╰────╯      │  45  46  47 ╭───────────╯
               ╰────────────╯                         ╰─────────────╯
 
-  ╭──────────────────────────────╮             ╭────╮      ╭──────────────────────────────╮
-  │ LT5  LT4  LT3  LT2  LT1  LT0 │         ╭───╯ J0 ╰───╮  │ RT0  RT1  RT2  RT3  RT4  RT5 │
-  │ LM5  LM4  LM3  LM2  LM1  LM0 │ ╭────╮  │ J1  J2  J3 │  │ RM0  RM1  RM2  RM3  RM4  RM5 │
-  │ LB5  LB4  LB3  LB2  LB1  LB0 │ │ E0 │  ╰───╮ J4 ╭───╯  │ RB0  RB1  RB2  RB3  RB4  RB5 │
-  ╰──────────────╮ LH2  LH1  LH0 │ ╰────╯      ╰────╯      │ RH0  RH1  RH2 ╭──────────────╯
-                 ╰───────────────╯                         ╰───────────────╯
+  ╭──────────────────────────────╮              ╭────╮      ╭──────────────────────────────╮
+  │ LT5  LT4  LT3  LT2  LT1  LT0 │          ╭───╯ J0 ╰───╮  │ RT0  RT1  RT2  RT3  RT4  RT5 │
+  │ LM5  LM4  LM3  LM2  LM1  LM0 │ ╭─────╮  │ J1  J2  J3 │  │ RM0  RM1  RM2  RM3  RM4  RM5 │
+  │ LB5  LB4  LB3  LB2  LB1  LB0 │ │ LEC │  ╰───╮ J4 ╭───╯  │ RB0  RB1  RB2  RB3  RB4  RB5 │
+  ╰──────────────╮ LH2  LH1  LH0 │ ╰─────╯      ╰────╯      │ RH0  RH1  RH2 ╭──────────────╯
+                 ╰───────────────╯                          ╰───────────────╯
 
 */
 
 #pragma once
-
-// numbering is from medial to lateral
 
 #define LT0  5  // left-top row
 #define LT1  4
@@ -71,10 +72,10 @@
 #define RH2 47
 
 #define J0 6
-#define J1 19 
+#define J1 19
 #define J2 20
 #define J3 21
 #define J4 35
 
-#define E0 34
+#define LEC 34
 
