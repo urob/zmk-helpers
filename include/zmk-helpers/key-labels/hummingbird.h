e45ce171c34dfecec4bbcecf5847891c1426a551
urob <978080+urob@users.noreply.github.com>
2026-03-04 16:55:09 -0500

    Fix hummingbird thumb labels

--- include/zmk-helpers/key-labels/hummingbird.h
+++ include/zmk-helpers/key-labels/hummingbird.h
@@ -4,7 +4,7 @@
   │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
   │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
   ╰───╮ 20  21  22     │     23  24  25 ╭───╯ ╰───╮ LB3 LB2 LB2     │     RB1 RB2 RB3 ╭───╯
-      ╰───────╮ 30  31 │ 32  33 ╭───────╯         ╰───────╮ LH1 LH0 │ RH0 RH1 ╭───────╯
+      ╰───────╮ 26  27 │ 28  29 ╭───────╯         ╰───────╮ LH1 LH0 │ RH0 RH1 ╭───────╯
               ╰────────┴────────╯                         ╰─────────┴─────────╯             */
 
 #pragma once
@@ -41,13 +41,13 @@
 #define RB2 24
 #define RB3 25
 
-#define LH0 31  // left thumb keys
-#define LH1 30
+#define LH0 27  // left thumb keys
+#define LH1 26
 
-#define RH0 32  // right thumb keys
-#define RH1 33
+#define RH0 28  // right thumb keys
+#define RH1 29
 
-#define NUMROW 
+#define NUMROW
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB1 LB2 LB3
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB1 RB2 RB3
 #define THUMBS_L LH0 LH1
